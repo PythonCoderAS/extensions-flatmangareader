bd4d35d3c434bd1a6607fa6db5d3de228344a042
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-05-02 16:41:20 -0400

    Add RawKuma

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Extensions for sources made with the FlatMangaReader template.
 ### Contains sources:  
 * [AsuraScans](https://www.asurascans.com)
 * [FlameScans](https://flamescans.org)
-* [KumaScans](https://kumascans.com/)
+* [KumaScans](https://kumascans.com)
 * [ManhwaX](https://manhwax.com)
+* [RawKuma](https://rawkuma.com) 
 * [ReadKomik](https://readkomik.com)
-* [SekteDoujin](https://75.119.132.111/)
+* [SekteDoujin](https://75.119.132.111)
---
+++ src/RawKuma/RawKuma.ts
@@ -0,0 +1,27 @@
+import {LanguageCode, SourceInfo, TagType} from "paperback-extensions-common";
+import {KumaScans} from "../KumaScans/KumaScans";
+
+const BASE = "https://rawkuma.com"
+
+export const RawKumaInfo: SourceInfo = {
+    icon: "icon.png",
+    version: "1.0.0",
+    name: "RawKuma",
+    author: "PythonCoderAS",
+    authorWebsite: "https://github.com/PythonCoderAS",
+    description: "Extension that pulls manga from RawKuma",
+    language: "_unknown",
+    hentaiSource: false,
+    websiteBaseURL: BASE,
+    sourceTags: [
+        {
+            text: "Notifications",
+            type: TagType.GREEN
+        }
+    ]
+}
+
+export class RawKuma extends KumaScans {
+    baseUrl: string = BASE;
+    language: LanguageCode = LanguageCode.UNKNOWN;
+}
